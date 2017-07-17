3f96f6dbf17052659cbd25e91462afd17f094a00
paweltar <paweltar@gmail.com>
2017-07-17 18:49:59 +0200

    Gallery section in progress

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,41 @@
-import UIkit from 'uikit';
-import Icons from 'uikit/dist/js/uikit-icons';
-import $ from 'jquery';
-import 'aos/dist/aos.css';
-import AOS from 'aos'
-import uikitCss from 'uikit/dist/css/uikit.css'
+import UIkit from "uikit";
+import Icons from "uikit/dist/js/uikit-icons";
+import $ from "jquery";
+// import "aos/dist/aos.css";
+// import AOS from "aos";
+import "uikit/dist/css/uikit.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel";
 
 UIkit.use(Icons);
 
-$(window).on("load", function(){
-  // AOS is just a great animation library. You can read more here https://github.com/michalsnik/aos 
-  AOS.init({
-      disable: window.innerWidth < 960,
-      offset: 200,
-      duration: 600,
-      easing: 'ease-in-sine',
+$(document).ready(function() {
+  $(".slides-container").slick({
+    infinite: true,
+    draggable: true,
+    arrows: false,
+    // prevArrow: ".slidenav--prev",
+    // nextArrow: ".slidenav--next",
+    speed: 600,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
-  console.log('hello');
 });
