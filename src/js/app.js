02204d6122139cd3f648d66128f18ee972659107
paweltar <paweltar@gmail.com>
2017-07-18 14:19:32 +0200

    Contact section in progress

--- src/js/app.js
+++ src/js/app.js
@@ -1,43 +1,13 @@
 import UIkit from "uikit";
 import Icons from "uikit/dist/js/uikit-icons";
 import $ from "jquery";
-// import "aos/dist/aos.css";
-// import AOS from "aos";
 import "uikit/dist/css/uikit.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel";
+import "./components/gallery-section";
+import "./components/contact-section";
 
 UIkit.use(Icons);
 
 $(document).ready(function() {
-  $(".slides-container").slick({
-    infinite: true,
-    draggable: true,
-    arrows: false,
-    dots: true,
-    speed: 600,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3500,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
-
   const mobileMenu = document.querySelector("#modal-full");
   mobileMenu.addEventListener("click", function() {
     UIkit.modal(this).hide();
---
+++ src/js/components/contact-section.js
@@ -0,0 +1,25 @@
+import $ from "jquery";
+import Leaflet from "leaflet/dist/leaflet.js";
+import LeafletCss from "leaflet/dist/leaflet.css";
+import iconImg from "leaflet/dist/images/marker-icon.png";
+import shadowImg from "leaflet/dist/images/marker-shadow.png";
+
+$(document).ready(function() {
+  this.customDefault = Leaflet.icon({
+    iconUrl: "js/" + iconImg,
+    shadowUrl: "js/" + shadowImg
+  });
+
+  var mymap = Leaflet.map("map-container").setView([51.505, -0.09], 13);
+
+  Leaflet.tileLayer("http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    maxZoom: 18,
+    detectRetina: true
+  }).addTo(mymap);
+
+  var marker = Leaflet.marker([51.505, -0.09], {
+    icon: this.customDefault
+  }).addTo(mymap);
+});
