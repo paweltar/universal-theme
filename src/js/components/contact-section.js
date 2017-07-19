d553ef7b3f13a49440b53e0834e0a26eac0641a5
paweltar <paweltar@gmail.com>
2017-07-19 15:17:07 +0200

    Switch loading of map tiles to https - quickfix

--- src/js/components/contact-section.js
+++ src/js/components/contact-section.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 
   var mymap = Leaflet.map("map-container").setView([51.505, -0.09], 13);
 
-  Leaflet.tileLayer("https://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png", {
+  Leaflet.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     maxZoom: 18,
